f0333479336ab1f9e75599c7110d850f48cac33d
Palntify <faisaljawed2000@gmail.com>
2023-05-28 16:02:38 +0500

    Work Accomplish

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,60 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-  MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.deepOrange,
-        title: Text("My First Flutter App"),
-      ),
-      body:Center(
-        child: Image(
-          image:NetworkImage('https://i.gifer.com/4Cb2.gif'),
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MyBulb();
+  }
+}
+
+class MyBulb extends StatefulWidget {
+  const MyBulb({super.key});
+
+  @override
+  State<MyBulb> createState() => _MyBulbState();
+}
+
+class _MyBulbState extends State<MyBulb> {
+  bool isTurnOn = true;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Text('Lets Glow'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                isTurnOn ? Icons.lightbulb : Icons.lightbulb_outline,
+                size: 200,
+                color: isTurnOn ? Colors.yellow : Colors.black,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isTurnOn = !isTurnOn;
+                  });
+                },
+                child: Text(!isTurnOn ? "Turn on" : "Turn off"),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  ),
-  );
+    );
+    
+  }
 }
